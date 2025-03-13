86076cfded20bb8050dfec849fcfd52de439975f
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-13 07:44:40 +0300

    set use state for updating a number increment

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import { useState } from 'react'
 
 function App () {
+  const [number, setNumber] = useState(1);
+
+  function handleClick () {
+    setNumber(number + 1);
+  }
   return (
     <div>
-      <h1>Good Morning</h1>
-      <button onClick={function () {
-        alert(`I just got clicked`);
-      }}>Increment</button>
+      <h1>Good Morning - {number}</h1>
+      <button onClick={handleClick}>Increment</button>
     </div>
   )
 }
